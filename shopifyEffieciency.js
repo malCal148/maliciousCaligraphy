07a343e9047113fc847ee702d961f544e0611386
Jeeves <157140834+malCal148@users.noreply.github.com>
2024-03-11 23:08:26 +0000

    A fuck ton of updates

--- shopifyEffieciency.js
+++
@@ -1,7 +0,0 @@
-function loadScript() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = scriptURL;
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-    script.onload = ShopifyBuyInit;
-  }
